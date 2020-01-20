46c237d84a3a60edb85ca68f7650f0d9878f9976
Jack Henahan <root@proofte.ch>
2020-01-19 19:48:09 -0500

    Get everything into the org file

--- early-init.el
+++ early-init.el
@@ -1,35 +1,49 @@
+;;; -*- lexical-binding: t; buffer-read-only: t -*-
 (defvar the--gc-cons-threshold gc-cons-threshold)
 (defvar the--gc-cons-percentage gc-cons-percentage)
 (defvar the--file-name-handler-alist file-name-handler-alist)
 
 (setq-default gc-cons-threshold 402653184
-              gc-cons-percentage 0.6
-              inhibit-compacting-font-caches t
-              message-log-max 16384
-              file-name-handler-alist nil)
+       gc-cons-percentage 0.6
+       inhibit-compacting-font-caches t
+       message-log-max 16384
+       file-name-handler-alist nil)
 
 (add-hook 'after-init-hook
-          (lambda ()
-            (setq gc-cons-threshold the--gc-cons-threshold
-                  gc-cons-percentage the--gc-cons-percentage
-                  file-name-handler-alist the--file-name-handler-alist)))
+   (lambda ()
+     (setq gc-cons-threshold the--gc-cons-threshold
+	   gc-cons-percentage the--gc-cons-percentage
+	   file-name-handler-alist the--file-name-handler-alist)))
 
+;; Use a hook so the message doesn't get clobbered by other messages.
+(add-hook 'emacs-startup-hook
+	  (lambda ()
+	    (message "Emacs ready in %s with %d garbage collections."
+		     (format "%.2f seconds"
+			     (float-time
+			      (time-subtract after-init-time before-init-time)))
+		     gcs-done)))
+
+(setq inhibit-startup-screen t)
 
 (if (fboundp 'tool-bar-mode)   (tool-bar-mode   -1))
 (if (fboundp 'menu-bar-mode)   (menu-bar-mode   -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
-(setq default-frame-alist '((font . "PragmataPro-14")
-                            (fullscreen . fullboth)
-                            (horizontal-scroll-bars)
-                            (vertical-scroll-bars)))
+(setq default-frame-alist '((font . "PragmataPro Liga-14")
+		     (fullscreen . fullboth)
+		     (horizontal-scroll-bars)
+		     (vertical-scroll-bars)))
 
 (when (featurep 'ns)
   (push '(ns-transparent-titlebar . t) default-frame-alist))
 
-(setq package-enable-at-startup nil)
-
 (setq frame-inhibit-implied-resize t)
+
+(require 'bytecomp)
+(byte-recompile-file (concat user-emacs-directory "pragmata.el") nil 0 t)
+
+(setq package-enable-at-startup nil)
 
 ;;; Set up package management
 ;;;; use-package
@@ -49,6 +63,9 @@
 
 (use-package no-littering :demand t)
 
+;; We can't tangle without org!
+(require 'org)
+
 (defun the-reload-and-tangle-init-org ()
     "Tangle and reload THE config."
     (interactive)
@@ -58,10 +75,8 @@
       (load (concat user-emacs-directory "init"))))   
 
 (add-to-list 'safe-local-variable-values
-             '(after-save-hook . the-reload-and-tangle-init-org))
+      '(after-save-hook . the-reload-and-tangle-init-org))
 
-;; We can't tangle without org!
-(require 'org)
 ;; Open the configuration
 (find-file (concat user-emacs-directory "init.org"))
 ;; tangle it
